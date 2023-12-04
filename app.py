075cd45dfd3670577f6da1fe7b4e39155e478e05
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2023-12-04 15:17:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 # Set page configuration and background image
 st.set_page_config(
-    page_title="Book Genre Insights",
+    page_title="Book Insights",
     page_icon=":books:",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -76,14 +76,91 @@ conn = snowflake.connector.connect(
 
 # Query to retrieve book data from Snowflake
 query = "SELECT * FROM BOOK_WEEK_3.KITAAB.BOOKS"
+query_total_books = "SELECT COUNT(*) AS TOTAL_BOOKS FROM BOOKS"
+query_best_category = (
+    "SELECT CATEGORY, COUNT(*) AS BOOK_COUNT "
+    "FROM BOOKS "
+    "WHERE RATING = 'FIVE' "
+    "GROUP BY CATEGORY "
+    "LIMIT 1"
+)
+query_books_categories =  "SELECT DISTINCT CATEGORY FROM BOOKS"
+
+# Function to fetch top 3 books for a selected category
+def fetch_top_books_for_category(selected_category):
+    # Snowflake query to fetch top 3 books in a selected category
+    query_top_books_for_category = (
+        f"SELECT CATEGORY, TITLE, RATING, PRICE, AVAILABILITY "
+        f"FROM ( "
+        f"   SELECT CATEGORY, TITLE, RATING, PRICE, AVAILABILITY, "
+        f"       ROW_NUMBER() OVER (PARTITION BY CATEGORY ORDER BY RATING DESC) AS rank "
+        f"   FROM BOOKS "
+        f"   WHERE CATEGORY = '{selected_category}'"
+        f") "
+        f"WHERE rank <= 3"
+    )
+
+    # Snowflake connection and data retrieval
+    conn = snowflake.connector.connect(
+    user=st.secrets['SNOWFLAKE_USER'],
+    password=st.secrets['SNOWFLAKE_PASSWORD'],
+    account=st.secrets['SNOWFLAKE_ACCOUNT'],
+    database=st.secrets['SNOWFLAKE_DATABASE'],
+    schema=st.secrets['SNOWFLAKE_SCHEMA']
+    )
+
+    # Fetching data into a Pandas DataFrame directly from Snowflake
+    top_books_for_category_df = pd.read_sql(query_top_books_for_category, conn)
+
+    # Close Snowflake connection
+    conn.close()
+
+    return top_books_for_category_df
+
+
 
 # Execute the query and fetch data into a Pandas DataFrame
 df = pd.read_sql(query, conn)
+# Fetching data into Pandas DataFrames directly from Snowflake
+total_books_df = pd.read_sql(query_total_books, conn)
+best_category_df = pd.read_sql(query_best_category, conn)
+categories_df = pd.read_sql(query_books_categories, conn)
 
-# if df['AVAILABILITY'].startswith
+# Extracting values from DataFrames
+total_books = total_books_df['TOTAL_BOOKS'][0] if not total_books_df.empty else 0
+
 
 # Close Snowflake connection
 conn.close()
+
+
+
+
+# Display KPIs using Streamlit components
+# st.title('Book Key Performance Indicators (KPIs)')
+
+# Total number of books displayed using st.info
+
+# col1 = st.columns(1)
+st.metric("Total Number of Books", total_books)
+# col2.metric("Total Number of Books", {total_books})
+
+# st.subheader('Total Number of Books')
+# st.info(f"Total Books: {total_books}")
+
+
+# st.subheader('Top 3 Books in Each Category')
+# Create a sidebar with a dropdown menu for selecting categories
+with st.sidebar:
+    selected_category = st.selectbox("Top 3 Books in Category:", categories_df['CATEGORY'])
+
+    # Display top 3 books for the selected category in the main content area
+    if selected_category:
+        st.header(f'Top 3 Books in Category: {selected_category}')
+        top_books_df = fetch_top_books_for_category(selected_category)
+        st.dataframe(top_books_df[['TITLE', 'RATING', 'PRICE', 'AVAILABILITY']])
+
+
 
 
 # Sorting options
